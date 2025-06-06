5707121868cd53b9c48bd31120e2cfffb50b2826
MateusRosa22 <mateus9898@gmail.com>
2025-06-06 14:36:24 -0300

    Adicionar arquivos de projeto.

---
+++ EX 7-8-9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SistemaMatriculas
+{
+    class Matricula
+    {
+        public string NomeDoAluno;
+        public string Curso;
+        public int NumeroMatricula;
+        public string Situacao;
+        public string DataInicial;
+
+        
+        public void Trancar()
+        {
+            Situacao = "Trancada";
+        }
+
+        
+        public void Reativar()
+        {
+            Situacao = "Ativa";
+        }
+        
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome do Aluno: {NomeDoAluno}");
+            Console.WriteLine($"Curso: {Curso}");
+            Console.WriteLine($"Número da Matrícula: {NumeroMatricula}");
+            Console.WriteLine($"Situação: {Situacao}");
+            Console.WriteLine($"Data Inicial: {DataInicial}");
+        }
+    }
+} 
